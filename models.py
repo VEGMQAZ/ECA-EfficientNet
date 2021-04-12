2e519358902ff1039f82943078c63a82cb2e8435
guangjinzheng <guangjinzheng@qq.com>
2021-04-12 22:41:23 +0800

    leafsnap data

--- models.py
+++ models.py
@@ -1,37 +1,36 @@
 from tensorflow.python.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications.resnet_v2 import ResNet101V2
-from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.applications.xception import Xception
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-from tensorflow.keras.applications.densenet import DenseNet201
-from tensorflow.keras.applications.densenet import DenseNet169
-from tensorflow.keras.applications.nasnet import NASNetMobile
+from tensorflow.python.keras.applications.resnet_v2 import ResNet101V2
+from tensorflow.python.keras.applications.inception_v3 import InceptionV3
+from tensorflow.python.keras.applications.xception import Xception
+from tensorflow.python.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.python.keras.applications.densenet import DenseNet201
+from tensorflow.python.keras.applications.densenet import DenseNet169
+from tensorflow.python.keras.applications.nasnet import NASNetMobile
 import tensorflow.python.keras.applications.efficientnet as efn
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
-from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, BatchNormalization, Flatten
+import tensorflow.keras.layers as layers
 import applications.efficientnet as eca
 import af   # hswish
 
 # AlexNet
 def myAlexNet(input_shape=(224, 224, 3), classes=1000):
-    inputs = Input(shape=input_shape)
-    c1 = Conv2D(48, (11, 11), strides=4, activation='relu', kernel_initializer='uniform', padding='valid')(inputs)
-    c2 = BatchNormalization()(c1)
-    c3 = MaxPool2D((3, 3), strides=2, padding='valid')(c2)
-    c4 = Conv2D(128, (5, 5), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c3)
-    c5 = BatchNormalization()(c4)
-    c6 = MaxPool2D((3, 3), strides=2, padding='valid')(c5)
-    c7 = Conv2D(192, (3, 3), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c6)
-    c8 = Conv2D(192, (3, 3), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c7)
-    c9 = Conv2D(128, (3, 3), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c8)
-    c10 = MaxPool2D((3, 3), strides=2, padding='valid')(c9)
-    c11 = Flatten()(c10)
-    c12 = Dense(2048, activation='relu')(c11)
-    c13 = Dropout(0.3)(c12)
-    c14 = Dense(2048, activation='relu')(c13)
-    c15 = Dropout(0.3)(c14)
-    outputs = Dense(classes, activation='softmax')(c15)
+    inputs = layers.Input(shape=input_shape)
+    c1 = layers.Conv2D(48, (11, 11), strides=4, activation='relu', kernel_initializer='uniform', padding='valid')(inputs)
+    c2 = layers.BatchNormalization()(c1)
+    c3 = layers.MaxPool2D((3, 3), strides=2, padding='valid')(c2)
+    c4 = layers.Conv2D(128, (5, 5), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c3)
+    c5 = layers.BatchNormalization()(c4)
+    c6 = layers.MaxPool2D((3, 3), strides=2, padding='valid')(c5)
+    c7 = layers.Conv2D(192, (3, 3), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c6)
+    c8 = layers.Conv2D(192, (3, 3), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c7)
+    c9 = layers.Conv2D(128, (3, 3), strides=1, padding='same', activation='relu', kernel_initializer='uniform')(c8)
+    c10 = layers.MaxPool2D((3, 3), strides=2, padding='valid')(c9)
+    c11 = layers.Flatten()(c10)
+    c12 = layers.Dense(2048, activation='relu')(c11)
+    c13 = layers.Dropout(0.3)(c12)
+    c14 = layers.Dense(2048, activation='relu')(c13)
+    c15 = layers.Dropout(0.3)(c14)
+    outputs = layers.Dense(classes, activation='softmax')(c15)
     model = Model(inputs=[inputs], outputs=[outputs])
     model.summary()
     return model
@@ -42,10 +41,10 @@ def myVGG16(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = VGG16(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = Flatten(name='flatten')(x)
-    x = Dense(4096, activation='relu', name='fc1')(x)
-    x = Dense(4096, activation='relu', name='fc2')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.Flatten(name='flatten')(x)
+    x = layers.Dense(4096, activation='relu', name='fc1')(x)
+    x = layers.Dense(4096, activation='relu', name='fc2')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -56,8 +55,8 @@ def myResNetV2101(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = ResNet101V2(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -68,8 +67,8 @@ def myInceptionV3(input_shape=(299, 299, 3), classes=1000):
     # pre_trained_model = InceptionV3(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -80,8 +79,8 @@ def myXception(input_shape=(299, 299, 3), classes=1000):
     # pre_trained_model = Xception(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -92,8 +91,8 @@ def myMobileNetV2(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = MobileNetV2(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -104,8 +103,8 @@ def myDenseNet201(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = DenseNet201(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -116,8 +115,8 @@ def myDenseNet169(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = DenseNet169(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -128,8 +127,8 @@ def myNASNetMobile(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = NASNetMobile(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     model.summary()
     return model
@@ -140,9 +139,9 @@ def myEfficientNetB0(input_shape=(224, 224, 3), classes=1000):
     # pre_trained_model = ef.EfficientNetB0(weights='imagenet', include_top=True)
     # pre_trained_model.summary()
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    x = Dropout(0.2, name='top_dropout')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dropout(0.2, name='top_dropout')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     # for layer in model.layers[:-6]:
     #     layer.trainable = False
@@ -162,9 +161,9 @@ def myEfficientNet(attention='se', activation='swish', input_shape=(224, 224, 3)
                 layeri.set_weights(temp)
         pre_trained_model = my_model
     x = pre_trained_model.output
-    x = GlobalAveragePooling2D(name='avg_pool')(x)
-    x = Dropout(0.2, name='top_dropout')(x)
-    predictions = Dense(classes, activation='softmax')(x)
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dropout(0.2, name='top_dropout')(x)
+    predictions = layers.Dense(classes, activation='softmax')(x)
     model = Model(inputs=pre_trained_model.input, outputs=predictions)
     # for layer in model.layers[:-6]:
     #     layer.trainable = False
--- test.py
+++ test.py
@@ -1,21 +1,99 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras_flops import get_flops
 import models
+import os
+import time
+import tensorflow as tf
+import numpy as np
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# Calculae FLOPS
-def getflops(model):
-    flops = get_flops(model, batch_size=1)
-    g = flops / 2.0 / 10 ** 9
-    # print(f"FLOPS: {flops / 10 ** 9:.03} G")
-    print('{:.3f}G'.format(g))
-    return g
+# EfficientNet B0 Tesgt data
+class Testefn(object):
+    def __init__(self, path='D:/deeplearning/datasets/imageclassification/', dataset='Flower', pathmodel='',
+                 attention='se', activation='swish', batch_size=32, input_shape=(224, 224, 3)):
+        self.datasets = os.listdir(path)
+        self.dataset = [dataseti for dataseti in self.datasets if dataset in dataseti][0]
+        self.path = '{}/{}/test/'.format(path, self.dataset)
+        self.classes = int(self.path.split('-')[-1].split('/')[0])
+        self.path_model = pathmodel
+        self.batch_size = batch_size
+        self.input_shape = input_shape
+        self.attention = attention
+        self.activation = activation
+        self.model = models.myEfficientNet(attention=self.attention, activation=self.activation,
+                                           input_shape=self.input_shape, classes=self.classes)
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(1e-5), loss='categorical_crossentropy',
+                           metrics='accuracy')
+        if self.path_model is not '':
+            self.model.load_weights(self.path_model)
+        self.test_data = ImageDataGenerator(rescale=1.0 / 255.).flow_from_directory(
+            self.path, batch_size=self.batch_size, class_mode='categorical',
+            target_size=(self.input_shape[0], self.input_shape[1]))
+
+    # Top1 Acc
+    def top1acc(self):
+        score = self.model.evaluate(self.test_data)
+        top1 = 100.0 * score[1]
+        print('Top1 Accuracy: {:.3f}%'.format(top1))
+        return top1
+
+    # Top5 Acc
+    def top5acc(self):
+        tf1 = 0
+        sum_times = 0
+        total = len(self.test_data.filepaths)
+        for i in range(total):
+            img_path = self.test_data.filepaths[i]
+            img = tf.keras.preprocessing.image.load_img(img_path, target_size=(self.input_shape[0], self.input_shape[1]))
+            x = tf.keras.preprocessing.image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = x / 255.0
+            ts = time.time()
+            preds = self.model.predict(x)[0]
+            tn = time.time()
+            sum_times += (tn - ts)
+            preclass = np.asarray(preds, np.float)
+            preflag = True
+            for j in range(5):
+                num = int(np.argmax(preclass))
+                preclass[num] = -1
+                # print('{} = {}'.format(j + 1, num))
+                if num == self.test_data.labels[i]:
+                    tf1 = tf1 + 1
+                    preflag = False
+                    break
+            if preflag:
+                print(img_path)
+                print('{}'.format(np.argmax(preds)))
+        print('true:{}  total:{}'.format(tf, total))
+        top5 = 100.0 * tf1 / total
+        print('Top5 Accuracy: {:.3f}%'.format(top5))
+        latency = 1000 * sum_times / total
+        return top5, latency
+
+    # Calculae FLOPs
+    def flops(self):
+        flops = get_flops(self.model, batch_size=1)
+        g = flops / 2.0 / 10 ** 9
+        print('{} {} {} {:.3f}G'.format(self.dataset, self.activation, self.attention, g))
+        return g
+
+    def all(self):
+        top1 = self.top1acc()
+        top5, latency = self.top5acc()
+        flop = self.flops()
+        self.model.summary()
+        print('{} {} {}'.format(self.dataset, self.activation, self.attention))
+        print('Top1: {:.3f}% Top5: {:.3f}% FLOPs: {:.3f}G FLOPs: {:.3f}ms'.format(top1, top5, flop, latency))
 
 if __name__ == '__main__':
-    hwd = 224
-    class_total = 131
-    at = 'eca'
-    af = 'hswish'
-    model = models.myEfficientNet(attention=at, activation=af, input_shape=(hwd, hwd, 3), classes=class_total)
-    flops = getflops(model)
-    print(flops)
+    arr_data = ['Leaf', 'Fruit', 'Flower']
+    arr_at = ['eca', 'se']
+    arr_af = ['hswish', 'swish', 'relu']
+    test = Testefn(dataset=arr_data[2], attention=arr_at[1], activation=arr_af[2],
+                   pathmodel='20210411-101023-0.105127-97.7451.h5')
+    flops = test.all()
+
 
 # 2021-04-09 guangjinzheng
